6d2d328214024c749aaa0ea346840ed4e01cf287
eirsture <eirik.sture@gmail.com>
2020-11-22 14:41:39 +0100

    add latex formula print outputs

--- b&s.py
+++ b&s.py
@@ -28,9 +28,12 @@ def first_determinant(stock_price, exercise_price, interest_rate, volatility, ti
     """
     d_1 = (ln(S_0 / X) + (r + 1/2 * sigma**2 ) * T) / (sigma * sqrt(T))
     """
-    det = (log(stock_price / exercise_price) + (interest_rate + 1/2 * volatility**2)*time) / (volatility * sqrt(time))
-    print(f'First determinant: {det}')
-    print(f'Formatted first determinant: {format(det, ".4f")} \n')
+    print("d_{1}=\\frac{\ln \left(S_{0} / X\\right)+\left(r+\\frac{1}{2} \sigma^{2}\\right) T}{\sigma \sqrt{T}}")
+    print("d_{1}=\\frac{\ln \left(%s / %s\\right)+\left(%s+\\frac{1}{2} %s^{2}\\right) %s}{%s \sqrt{%s}}" % (
+    stock_price, exercise_price, interest_rate, volatility, time, volatility, time))
+
+    det = float(format((log(stock_price / exercise_price) + (interest_rate + 1/2 * volatility**2)*time) / (volatility * sqrt(time)), ".4f"))
+    print('d_{1}= %s \n\n' % (det))
     return det
 
 
@@ -38,9 +41,11 @@ def second_determinant(determinant1, volatility, time) -> float:
     """
     d_2 = d_1 - sigma * sqrt(T)
     """
-    det = determinant1 - volatility * sqrt(time)
-    print(f'Second determinant: {det}')
-    print(f'Formatted second determinant: {format(det, ".4f")} \n')
+    print("d_{2}= d_{1} - \sigma \sqrt{T}")
+    print("d_{2}= %s - %s \sqrt{%s}" % (determinant1, volatility, time))
+
+    det = float(format(determinant1 - volatility * sqrt(time), ".4f"))
+    print('d_{2}= %s \n\n' % (det))
     return det
 
 
@@ -52,23 +57,33 @@ def put_option(exercise_price, interest_rate, time, stock_price, determinant1, d
     """
      O_p,0 = Xe**(−rT) *  N(−d2) − S_0 * N(−d1)
     """
+    print("O_{p, 0}=X \cdot e^{-r \cdot T} \cdot N\left(-d_{2}\\right)-S_{0} \cdot N\left(-d_{1}\\right)")
+    print("O_{p, 0}=%s \cdot e^{-%s \cdot %s} \cdot N\left(-%s\\right)-%s \cdot N\left(-%s\\right)" % (
+        exercise_price, interest_rate, time, determinant2, stock_price, determinant1))
 
-    return exercise_price * exp(-interest_rate * time) * normal_distribution(
-        -determinant2) - stock_price * normal_distribution(-determinant1)
+    option_price = float(format(exercise_price * exp(-interest_rate * time) * normal_distribution(
+        -determinant2) - stock_price * normal_distribution(-determinant1), ".4f"))
+    print('O_{p, 0}= %s \n\n' % (option_price))
+    return option_price
 
 
 def call_option(exercise_price, interest_rate, time, stock_price, determinant1, determinant2) -> float:
     """
     O_c,0 = S_0 * N(d_1) - X*e^(-rT) * N(d_2)
     """
+    print("O_{c, 0}=S_{0} \cdot N(d_{1}) - X \cdot e^{-r \cdot T} \cdot N(d_{2})")
+    print("O_{c, 0}=%s \cdot N(%s) - %s \cdot e^{-%s \cdot %s} \cdot N(%s)" % (
+        stock_price, determinant1, exercise_price, interest_rate, time, determinant2))
 
-    return stock_price * normal_distribution(determinant1) - exercise_price * exp(
-        -interest_rate * time) * normal_distribution(determinant2)
+    option_price = float(format(stock_price * normal_distribution(determinant1) - exercise_price * exp(
+        -interest_rate * time) * normal_distribution(determinant2), ".4f"))
+    print('O_{c, 0}= %s \n\n' % (option_price))
+    return option_price
 
 
 if __name__ == '__main__':
     #  Input values:
-    option_type = "put"  # "put" or "call"
+    option_type = "call"  # "put" or "call"
     S_0 = None  # Stock price at time 0
     X = None  # Exercise price
     sigma = None  # Diffusion coefficient, representing the stock's volatility
@@ -80,7 +95,6 @@ if __name__ == '__main__':
     d_2 = second_determinant(d_1, sigma, T)
     if not (option_type == "put" or option_type == "call"):
         print(f'{option_type} is an invalid option type. Legal values are ("put", "call")')
-        option_price = ''
     elif option_type == "put":
         print("Calculating put option:")
         option_price = put_option(X, r, T, S_0, d_1, d_2)
@@ -89,6 +103,3 @@ if __name__ == '__main__':
         option_price = call_option(X, r, T, S_0, d_1, d_2)
     else:
         print("You should not be here")
-
-    print(f'Option price: {option_price}')
-    print(f'Formatted option price: {format(option_price, ".4f")}')
--- general.py
+++ general.py
@@ -1,20 +1,22 @@
 def print_num(num):
     return format(num, ".4f")
 
-def capm(r_f, r_m, b_p):
-    r_p = r_f + (r_m-r_f)*b_p
-    print(f'r_p from CAPM: {print_num(r_p)}')
+def capm(r_f, r_m, b_p, variable_name="p"):
+    print("r_{%s}=r_{f}+\left(r_{m}-r_{f}\\right) \cdot \\beta_{%s}" % (variable_name, variable_name))
+    print("r_{%s}=%s+\left(%s-%s\\right) \cdot %s" % (variable_name, r_f, r_m, r_f, b_p))
+
+    r_p = float(format(r_f + (r_m-r_f)*b_p, ".4f"))
+    print("r_{%s} = %s" % (variable_name, r_p))
     return r_p
 
-
-def wacc(r_d, r_e, D, E, V, tax):
-    wacc = r_e*(E/V) + r_d*(1-tax)*(D/V)
+def wacc_after_tax(r_d, r_e, D, E, V, tax):
+    wacc = float(format(r_e*(E/V) + r_d*(1-tax)*(D/V), ".4f"))
     print(f'WACC: {print_num(wacc)}')
     return wacc
 
 if __name__ == '__main__':
-    r_f = None  # Risk-free interest rate
-    r_m = None  # Return of market
-    b_p = None  # Beta of return you need calculated, e.g. asset
+    r_f = 1  # Risk-free interest rate
+    r_m = 1  # Return of market
+    b_p = 1  # Beta of return you need calculated, e.g. asset
 
-    capm(r_f, r_m, b_p)
+    capm(r_f, r_m, b_p, variable_name="a")
